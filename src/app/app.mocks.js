8315f52ec1dee6c71b76047d4123384ef09ccfff
Tuan Tuan LE <tlcong@linagora.com>
2020-09-07 16:07:13 +0700

    https://github.com/OpenPaaS-Suite/esn-frontend-account/issues/60 mocking sanitizeStylisedHtml filter should return a function

--- src/app/app.mocks.js
+++ src/app/app.mocks.js
@@ -5,6 +5,6 @@ const noop = angular.noop;
 angular.module('esn.inbox.libs', [])
   .service('jmapDraft', noop)
   .filter('sanitizeStylisedHtml', function() {
-    return '';
+    return noop;
   });
 angular.module('linagora.esn.unifiedinbox', []);
